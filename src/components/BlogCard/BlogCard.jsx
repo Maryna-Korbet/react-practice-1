72aa7a7443cc26baed98a6d3ce66e9d7c682d96e
Maryna-Korbet <m22v86k@gmail.com>
2024-02-06 18:27:36 +0200

    added task#1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,28 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import { formatDateToNow } from "helpers/format-date";
+import { Card, CardHeader, CardPoster, CardBody, Tag, CardTitle, CardText,  CardFooter, UserBox, Avatar, UserInfo, UserName, Date} from "./BlogCard.styled";
+
+export const BlogCard = ({name, postedAt, poster, tag, title, description, avatar}) => {
+  return <Card>
+  <CardHeader>
+    <CardPoster
+      src={poster}     
+      alt={tag}
+    />
+  </CardHeader>
+  <CardBody>
+    <Tag>{tag}</Tag>
+    <CardTitle>{title}</CardTitle>
+    <CardText>{description}</CardText>
+  </CardBody>
+  <CardFooter>
+    <UserBox>
+      <Avatar src={avatar} alt={name} />
+      <UserInfo>
+        <UserName>{name}</UserName>
+        <Date>{formatDateToNow(postedAt)}</Date>
+      </UserInfo>
+    </UserBox>
+  </CardFooter>
+</Card>
 };
+
---
+++ src/helpers/format-date.js
@@ -0,0 +1,6 @@
+import { formatDistanceToNow } from "date-fns";
+
+export function formatDateToNow(date) {
+    return formatDistanceToNow(new Date(date), { addSuffix: true })
+};
+
